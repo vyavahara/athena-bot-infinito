7e52bac6d082413000d90cfcfa203c9b7aac6d00
vyavahara <borghimarina@gmail.com>
2026-07-21 12:38:58 +0200

    Update app.py

--- app.py
+++ app.py
@@ -47,7 +47,7 @@ if prompt := st.chat_input("Fai la tua domanda ad Athena..."):
 
     try:
         response = client.models.generate_content(
-            model="gemini-1.5-flash",
+            model="gemini-1.5-flash-latest",
             contents=contents_history,
             config=types.GenerateContentConfig(
                 system_instruction=prompt_colonna,
